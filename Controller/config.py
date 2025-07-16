95ef567ba86d9c0cc40b02129813d872b12fb577
ChoJiaHong <63405306+ChoJiaHong@users.noreply.github.com>
2025-07-17 02:53:49 +0800

    refactor: modularize controller utilities

---
+++ Controller/config.py
@@ -0,0 +1,7 @@
+GPU_MEMORY_LABEL = "nvidia.com/gpu.memory"
+IN_CLUSTER = True
+SERVICE_FILE = './information/service.json'
+SERVICESPEC_FILE = './information/serviceSpec.json'
+SUBSCRIPTION_FILE = './information/subscription.json'
+NODE_STATUS_FILE = './information/nodestatus.json'
+LOG_FILE = './logdir/controller.log'
